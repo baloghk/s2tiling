08d01512cb14025a9204c35dbe6168ee55b2c5de
Balogh Károly <balogh2k4@gmail.com>
2025-12-05 01:53:40 +0100

    chore: bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="s2tiling",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     include_package_data=True,
     package_data={
